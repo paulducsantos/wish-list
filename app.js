b23c1c36936023bad7c453b4cd61aefd5760abbe
Paul Santos <paulmichael.a.santos@gmail.com>
2016-02-10 20:18:38 -0500

    fixed layout default

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var exphbs  = require('express-handlebars');
 const PORT = process.env.PORT || 8080;
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'wishlist'}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 var mysql      = require('mysql');
