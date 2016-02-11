573c8ce55f43c4e799db49941f968299ae3f5ada
d1son <dson120@gmail.com>
2016-02-10 19:58:35 -0500

    created path for the database wish-list

--- app.js
+++ app.js
@@ -3,5 +3,28 @@ var exphbs  = require('express-handlebars');
 const PORT = process.env.PORT || 8080;
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({defaultLayout: 'wishlist'}));
 app.set('view engine', 'handlebars');
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'local',
+  user     : 'root',
+  database : 'wish-list'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+ 
+  console.log('The solution is: ', rows[0].solution);
+});
+
+app.get("/" function (req, res) {
+	res.rend("wishlist")
+});
+
+app.listen(PORT, function() {
+	console.log("Server is listening on %s", PORT);
+});
